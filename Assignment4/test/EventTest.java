8da383f158e157e08c51112a7dc4cae3589065c9
MrRamen <kennyl0732@gmail.com>
2025-06-06 16:36:19 -0400

    stuff

--- Assignment4/test/EventTest.java
+++ Assignment4/test/EventTest.java
@@ -1,4 +1,23 @@
+import org.junit.Test;
+
+import calendar.IEvent;
+import calendar.SeriesEvent;
+
 import static org.junit.Assert.*;
+
 public class EventTest {
-  
+  private IEvent testEvent;
+
+  @Test
+  public void testSingleEvent() {
+
+  }
+
+  @Test
+  public void testSeriesEvent() {
+    SeriesEvent testSeriesEvent = new SeriesEvent("OOD", "2025-01-01T08:08",
+            "2025-01-20T10:10",
+            "WRS", "2025-01-25");
+  }
+
 }
