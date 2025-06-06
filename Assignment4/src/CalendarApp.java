8dfa484eefa0b7286ec12a165619e145c7570678
kellne-a <kellner.a@northeastern.edu>
2025-06-06 19:38:42 -0400

    controller, view, main done

---
+++ Assignment4/src/CalendarApp.java
@@ -0,0 +1,35 @@
+import java.io.File;
+
+import calendar.Calendar;
+import calendar.ICalendar;
+import controller.Controller;
+import controller.IController;
+import view.IView;
+import view.TextView;
+
+/**
+ * Class to run calendar from.
+ */
+public class CalendarApp {
+  /**
+   * Runs calendar from the terminal.
+   *
+   * @param args String[] from terminal
+   */
+  public static void main(String[] args) {
+    if (args.length < 3) {
+      throw new IllegalArgumentException("Invalid number of arguments");
+    }
+    ICalendar calendar = new Calendar("2025-06-06");
+    IView view = new TextView(calendar);
+    IController controller = new Controller(calendar, view);
+    if (args[2].equals("interactive")) {
+      controller.goInteractiveCalendar();
+    } else if (args[2].equals("headless")) {
+      if (args.length != 4) {
+        throw new IllegalArgumentException("Invalid number of arguments");
+      }
+      controller.goHeadlessCalendar(new File(args[3]));
+    }
+  }
+}
--- Assignment4/src/view/IView.java
+++ Assignment4/src/view/IView.java
@@ -1,4 +1,14 @@
 package view;
 
+/**
+ * Interface for all views. All views can display messages.
+ */
 public interface IView {
+
+  /**
+   * Returns the log.
+   *
+   * @return String
+   */
+  public String display(String message);
 }
